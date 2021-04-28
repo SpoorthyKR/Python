62a35101300adcd1e6aa3bf1380886e84a551811
Spoorthy Rajashekar <57374095+SpoorthyKR@users.noreply.github.com>
2021-04-28 13:36:50 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Python program to check if the input number is odd or even.
+# A number is even if division by 2 gives a remainder of 0.
+# If the remainder is 1, it is an odd number.
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
+
+# Taking kilometers input from the user
+kilometers = float(input("Enter value in kilometers: "))
+
+# conversion factor
+conv_fac = 0.621371
+
+# calculate miles
+miles = kilometers * conv_fac
+print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
